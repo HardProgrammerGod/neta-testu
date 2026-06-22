f5031f8e9500403d68ba950f687e39cdbc4d6392
Hard <herdbuilding1@gmail.com>
2026-06-22 18:25:19 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+def get_int(name: str, default=0):
+    try:
+        return int(os.getenv(name, default))
+    except:
+        return default
+
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 SUPABASE_URL = os.getenv("SUPABASE_URL")
 SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
-ADMIN_ID = int(os.getenv("ADMIN_ID"))
-PORT = int(os.getenv("PORT", 8000))
+
+CHANNEL_ID = get_int("CHANNEL_ID")
+ADMIN_ID = get_int("ADMIN_ID")
+PORT = get_int("PORT", 8000)
