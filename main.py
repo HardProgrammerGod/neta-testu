70774375487c34189d7a67ba4f3b8512d2388768
Hard <herdbuilding1@gmail.com>
2026-06-26 14:01:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,32 +3,50 @@ from aiogram import Bot, Dispatcher
 from aiohttp import web
 from config import BOT_TOKEN, PORT
 from handlers import start, quiz, admin, profile
+from middlewares.throttling import ThrottlingMiddleware
 
+# иниц бота
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+# защитка от спама кнопками
+dp.callback_query.middleware(ThrottlingMiddleware(slow_down_rate=0.6))
+
+# роутеры бота
 dp.include_router(start.router)
 dp.include_router(quiz.router)
 dp.include_router(admin.router)
 dp.include_router(profile.router)
-router.callback_query.middleware(ThrottlingMiddleware())
 
+
+# для оживки сервер для аптайм
 async def handle_ping(request):
     return web.Response(text="NetaNMT is actively running!")
+
 
 async def start_web_server():
     app = web.Application()
     app.router.add_get("/", handle_ping)
     runner = web.AppRunner(app)
     await runner.setup()
+    # сервер аптаймера 
     site = web.TCPSite(runner, "0.0.0.0", PORT)
     await site.start()
 
+# врубка бота
 async def main():
-    # Запуск легкого веб-сервера, щоб Render не вимикав сервіс
-    await start_web_server()
-    # Запуск лонг-полінгу бота
+    # веб сервер чтобы бот не падал
+    await start_web_server()  
+    # очистка старых соо перед полингом
+    await bot.delete_webhook(drop_pending_updates=True)   
+    # запуск лонг полинга
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    async def run():
+        try:
+            await main()
+        except (KeyboardInterrupt, SystemExit):
+            pass
+
+    asyncio.run(run())
